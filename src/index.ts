edf8fa0fc92ee2d1caf285bcd7ae5266f4e66492
Moksleivis <Moksleivis@kitm.local>
2026-04-23 11:11:09 +0300

    sql

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,32 @@ const server = serve({
 });
 
 console.log(`🚀 Server running at ${server.url}`);
+
+
+import { sql, SQL} from "bun";
+
+const db = new SQL("BunTemplate\src\database.sqlite");
+
+await db`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    name TEST NOT NULL,
+    email TEXT NOT NULL UNIQUE
+)
+`;
+
+async function createUser(name, emailas) {
+    const[newUser] = await db`
+    INSERT INTO users ${sql({
+        name,
+        email: emailas
+    })}
+    RETURNING *
+    `;
+
+    return newUser;
+}
+
+const user = await createUser("Alice", "alice2@example.com");
+
+console.log(user);
